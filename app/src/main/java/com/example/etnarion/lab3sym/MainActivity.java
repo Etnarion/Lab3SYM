51c79fdad2002fc4c77a4fe45b18cf71d9b445b7
Etnarion <samuelmayorweb@gmail.com>
2018-12-10 11:10:58 +0100

    initial commit

---
+++ app/src/main/java/com/example/etnarion/lab3sym/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.etnarion.lab3sym;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
